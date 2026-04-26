b4fbd8ab50301b34e784d7837c89b120e7df9154
makoto2025 <aratamakoto2025@gmail.com>
2026-04-25 22:12:39 -0700

    ex03

--- exercises/ex03.ts
+++ exercises/ex03.ts
@@ -2,9 +2,11 @@
 // Use function overloading to define the function in two variants: one for numbers and another for strings.
 // Return the string.
 
-const combine = () => {
-  //Code here
-};
+
+
+function combine(val1: number | string, val2: number | string): string {
+  return String(val1) + String(val2);
+}
 
 console.log(combine(2, 3)); // Expected output: "23"
 console.log(combine("Hello, ", "world!")); // Expected output: "Hello, world!"
