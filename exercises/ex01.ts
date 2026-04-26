a7991aebcd29e6a6bbc2d045d0fa1be3078a634c
makoto2025 <aratamakoto2025@gmail.com>
2026-04-25 22:01:32 -0700

    ex02

--- exercises/ex01.ts
+++ exercises/ex01.ts
@@ -1,17 +1,42 @@
 // Create a function that makes mathematical operations dynamically.
-// This function should receive as param a string or number, number and string
-// First param: string or number (mandatory)
-// Second param: number (optional)
-// Third param: Enum (optional) - '+', '-', '/', '*'
+// // This function should receive as param a string or number, number and string
+// //   First param: string or number (mandatory)
+// //   Second param: number (optional)
+// //   Third param: Enum (optional) - '+', '-', '/', '*'
 // If the first param is string, just return it. Otherwise, use the two numbers and the string to make the math operation
 // Eg: printInfoOrCalculate(3,5,"+") //Expected result: 8
 // Eg: printInfoOrCalculate(5,3,"-") //Expected result: 2
 // Eg: printInfoOrCalculate("TS",5,"+") //Expected result: TS
 // Eg: printInfoOrCalculate("TS") //Expected result: TS
 
-const printInfoOrCalculate = () => {
-  // Code here
-};
+type Operation = "+" | "-" | "/" | "*";
+
+function printInfoOrCalculate(
+	param1: string | number,
+	param2?: number,
+	param3?: Operation,
+): string | number {
+	if (typeof param1 === "string") {
+		return param1;
+	}
+
+	if (typeof param1 === "number" && typeof param2 === "number" && param3) {
+		switch (param3) {
+			case "+":
+				return param1 + param2;
+			case "-":
+				return param1 - param2;
+			case "*":
+				return param1 * param2;
+			case "/":
+				return param1 / param2;
+			default:
+				return "Invalid input";
+		}
+	}
+
+	return "Missing parameters for calculation";
+}
 
 // Tests
 console.log(printInfoOrCalculate(3, 5, "+")); // Expected result: 8
