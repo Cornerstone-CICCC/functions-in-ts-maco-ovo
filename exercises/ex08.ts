90e14ba58c55bcd4b4089166d9f4af9bd0e35dab
makoto2025 <aratamakoto2025@gmail.com>
2026-04-28 10:48:22 -0700

    ex08

--- exercises/ex08.ts
+++ exercises/ex08.ts
@@ -1,10 +1,29 @@
 // Create an array of product objects, each with their name, price, and quantity sold.
 // Additionally, you are given a tax rate as a percentage.
-// Write a function called calculateTotalSalesWithTax that takes in an array of product objects, along with the tax rate, and returns the total sales amount including tax.
+// Write a function called calculateTotalSalesWithTax 
+// that takes in an array of product objects, along with the tax rate, and returns the total sales amount including tax.
 
-const calculateTotalSalesWithTax = () => {
-  // Code here
+interface Product {
+  name: string;
+  price: string; 
+  quantitySold: number;
 };
+
+const calculateTotalSalesWithTax = (products: Product[], taxRate: number): string => {
+  
+  const subtotal = products.reduce((sum, product) => {    
+    const numPrice = parseFloat(product.price.replace(/[^\d.]/g, ""));
+    return sum + (numPrice * product.quantitySold);
+  }, 0);
+
+  const total = subtotal * (1 + (taxRate / 100));
+
+  return `CAD ${total.toFixed(2)}`;
+};
+
+
+
+
 
 export default calculateTotalSalesWithTax;
 
