e1dce65ab5f02f51f80256f28ee63c87f8cd8b05
makoto2025 <aratamakoto2025@gmail.com>
2026-04-28 10:37:11 -0700

    ex07

--- exercises/ex07.ts
+++ exercises/ex07.ts
@@ -1,8 +1,34 @@
 // Use the `typeof` operator to create a function that dynamically determines the type of the argument and logs it.
 // Return a string;
 
-const logType = () => {
-  // Code here.
+const logType = (log: any): string => {
+	// an array ?
+	if (Array.isArray(log)) {
+		if (log.length === 0) {
+			return `The type of the argument is: empty array`;
+		}
+
+    // All same type
+		const firstType = typeof log[0];
+		const allSameType = log.every((item) => typeof item === firstType);
+
+		if (allSameType) {
+			if (firstType === "number") {
+				return `The type of the argument is: array of number`;
+			} else if (firstType === "string") {
+				return `The type of the argument is: array of string`;
+			} else if (firstType === "boolean") {
+				return `The type of the argument is: array of boolean`;
+			}
+		}
+
+		// Mixed 
+		return `The type of the argument is: array`;
+	}
+
+	// non-array
+	const type = typeof log;
+	return `The type of the argument is: ${type}`;
 };
 
 // Test cases:
